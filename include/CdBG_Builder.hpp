4806e7aa52b8f2aee782e98998c35de0d454fc40
md-jamshed-khan <jamshed@umd.edu>
2020-05-24 21:55:00 -0400

    Remove some code no longer required

--- include/CdBG_Builder.hpp
+++ include/CdBG_Builder.hpp
@@ -61,37 +61,6 @@ private:
 
 
 
-    // =========================================================================== //
-
-    // Processes classification (partially) for the first k-mer of reference `ref`.
-    // void process_first_kmer(const std::string& ref);
-
-    // // Processes classification (partially) for the last k-mer of reference `ref`.
-    // void process_last_kmer(const std::string& ref);
-
-    // // Processes classification (partially) for an internal k-mer of reference
-    // // `ref`, at index `kmer_idx`.
-    // void process_internal_kmer(const std::string& ref, const uint32_t kmer_idx);
-
-    // // Returns a Boolean denoting whether the k-mer `kmer` at index `kmer_idx` of
-    // // reference `ref` forms a self loop with its next k-mer in the reference
-    // // (i.e. k-mer at idx `kmer_idx` + 1). Expects that `kmer` is not the last
-    // // k-mer of 'ref`.
-    // bool is_self_loop(const std::string& ref, const cuttlefish::kmer_t& kmer, const uint32_t kmer_idx) const;
-
-    // // Outputs the unitig at the index range [`start_idx`, `end_idx`] of reference
-    // // `ref` (if the unitig had not been output already), to the stream `output`.
-    // void output_unitig(const std::string& ref, const uint32_t start_idx, const uint32_t end_idx, std::ofstream& output);
-
-    // // Returns a Boolean denoting whether the k-mer at index `kmer_idx` of
-    // // reference `ref` starts a maximal unitig.
-    // bool is_unipath_start(const std::string& ref, const uint32_t kmer_idx) const;
-
-    // // Returns a Boolean denoting whether the k-mer at index `kmer_idx` of reference
-    // // `ref` ends a maximal unitig.
-    // bool is_unipath_end(const std::string& ref, const uint32_t kmer_idx) const;
-
-
 public:
     CdBG_Builder()
     {}

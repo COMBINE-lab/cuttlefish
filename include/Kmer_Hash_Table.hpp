d2d257876a50512a22d46984f06a9cfda5626c6d
md-jamshed-khan <jamshed@umd.edu>
2020-06-01 20:59:50 -0400

    Rename and modify k-mer hash table class.

--- include/Kmer_Hash_Table.hpp
+++ include/Kmer_Hash_Table.hpp
@@ -1,12 +1,12 @@
 
-#ifndef KMER_HASHTABLE_HPP
-#define KMER_HASHTABLE_HPP
+#ifndef KMER_HASH_TABLE_HPP
+#define KMER_HASH_TABLE_HPP
 
 
 #include "globals.hpp"
 #include "Kmer.hpp"
 #include "Vertex_Encoding.hpp"
-#include <BBHash/BooPHF.h>
+#include "BBHash/BooPHF.h"
 
 #include <map>
 #include <vector>
@@ -19,7 +19,7 @@ typedef boomphf::SingleHashFunctor<u_int64_t> hasher_t;
 typedef boomphf::mphf<u_int64_t, hasher_t> boophf_t;
 
 
-class Kmer_HashTable
+class Kmer_Hash_Table
 {
 private:
     // Lowest bits/elem is achieved with gamma = 1, higher values lead to larger mphf but faster construction/query.
@@ -34,11 +34,12 @@ private:
 
 
 public:
-    Kmer_HashTable()
+    Kmer_Hash_Table()
     {}
 
-    // Constructs a minimal perfect hash for the k-mers collection stored
-    // in the file named `kmers_file`, that has `kmer_count` number of k-mers.
+    // Constructs a minimal perfect hash function for the collection of k-mers
+    // (in 64-bits format) present in the file named `kmers_file`, that has
+    // `kmer_count` number of k-mers.
     void construct(const std::string& kmers_file, const uint64_t kmer_count);
 
     // Returns a mutable reference to the value (in the hash-table) for the
@@ -54,13 +55,13 @@ public:
 
 
 
-inline Vertex_Encoding& Kmer_HashTable::operator [](const cuttlefish::kmer_t& kmer)
+inline Vertex_Encoding& Kmer_Hash_Table::operator [](const cuttlefish::kmer_t& kmer)
 {
     return hash_table[mph->lookup(kmer.int_label())];
 }
 
 
-inline const Vertex_Encoding Kmer_HashTable::operator [](const cuttlefish::kmer_t& kmer) const
+inline const Vertex_Encoding Kmer_Hash_Table::operator [](const cuttlefish::kmer_t& kmer) const
 {
     return hash_table[mph->lookup(kmer.int_label())];
 }
--- src/Kmer_Hash_Table.cpp
+++ src/Kmer_Hash_Table.cpp
@@ -1,12 +1,12 @@
 
-#include "Kmer_HashTable.hpp"
+#include "Kmer_Hash_Table.hpp"
 
 #include <fstream>
 #include <vector>
 #include <chrono>
 
 
-void Kmer_HashTable::construct(const std::string& kmers_file, const uint64_t kmer_count)
+void Kmer_Hash_Table::construct(const std::string& kmers_file, const uint64_t kmer_count)
 {
     std::chrono::high_resolution_clock::time_point t_start = std::chrono::high_resolution_clock::now();
 
@@ -21,21 +21,29 @@ void Kmer_HashTable::construct(const std::string& kmers_file, const uint64_t kme
 
     
     // Get the k-mers collection into memory.
+    // TODO: Replace this mechanism totally.
     std::vector<uint64_t> kmers;
     kmers.reserve(kmer_count);
     
     uint64_t kmer;
     while(input >> kmer)
         kmers.push_back(kmer);
-    
+
+
+    // Number of threads to use in the MPH build.
+    // TODO: fix this.
+    const uint32_t thread_count = 16;
 
     // Build the MPHF.
-    mph = new boomphf::mphf<u_int64_t, hasher_t>(kmer_count, kmers, 16, gamma_factor);
-
-    std::cout << "MPH table size in bits / elem: " << (float)(mph->totalBitSize()) / kmer_count;
+    mph = new boomphf::mphf<u_int64_t, hasher_t>(kmer_count, kmers, thread_count, gamma_factor);
 
 
-    // Remove the k-mers from memory forcefully.
+    const uint64_t total_bits = mph->totalBitSize();
+    std::cout << "Total MPH size (in MB): " << total_bits / (8 * 1024 * 1024) << "\n";
+    std::cout << "MPH table size in bits / elem: " << (float)(total_bits) / kmer_count << "\n";
+
+
+    // Remove the k-mers collection from memory forcefully.
     kmers.clear();
     kmers.shrink_to_fit();
 
@@ -46,11 +54,11 @@ void Kmer_HashTable::construct(const std::string& kmers_file, const uint64_t kme
 
     std::chrono::high_resolution_clock::time_point t_end = std::chrono::high_resolution_clock::now();
     double elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double>>(t_end - t_start).count();
-    std::cout << "\nDone constructing the MPH table. Time taken = " << elapsed_seconds << " seconds.\n";
+    std::cout << "Done constructing the MPH table. Time taken = " << elapsed_seconds << " seconds.\n";
 }
 
 
-void Kmer_HashTable::clear()
+void Kmer_Hash_Table::clear()
 {
     // hash.clear();
     delete mph;

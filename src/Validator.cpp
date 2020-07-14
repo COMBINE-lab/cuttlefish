bccd3e34d3d660cc98e301fd689a5b4733458810
jamshed-k <jamshed@umd.edu>
2020-07-13 22:58:35 -0400

    Refactor validator

--- src/Validator.cpp
+++ src/Validator.cpp
@@ -27,10 +27,10 @@ bool Validator::validate(const std::string& bbhash_file_name, const uint16_t thr
 {
     build_mph_function(bbhash_file_name, thread_count);
 
-    bool valid_kmer_set = validate_kmer_set();
+    const bool valid_kmer_set = validate_kmer_set();
     std::cout << (valid_kmer_set ? "Passed" : "Failed") << " validation of the k-mer set.\n";
 
-    bool valid_sequence = validate_sequence_completion();
+    const bool valid_sequence = validate_sequence_completion();
     std::cout << (valid_sequence ? "Passed" : "Failed") << " validation of complete coverage of the sequence"
                                                            " by the produced unitigs.\n";
 
@@ -61,7 +61,7 @@ void Validator::build_mph_function(const std::string& bbhash_file_name, const ui
         std::cout << "Building the MPH function from the k-mer database " << kmer_container.container_location() << "\n";
 
         auto data_iterator = boomphf::range(kmer_container.begin(), kmer_container.end());
-        mph = new boomphf::mphf<cuttlefish::kmer_t, Kmer_Hasher> (kmer_container.size(), data_iterator, thread_count, 2);
+        mph = new boomphf::mphf<cuttlefish::kmer_t, Kmer_Hasher> (kmer_container.size(), data_iterator, thread_count, gamma_factor);
 
         std::cout << "Built the MPH function in memory.\n";
         
@@ -82,8 +82,8 @@ bool Validator::validate_kmer_set() const
 {
     std::cout << "Testing validation of the uniqueness of the k-mers and completeness of the k-mer set in the produced unitigs.\n";
 
-    Kmer_Container kmer_container(kmc_db_name);
-    uint64_t kmer_count = kmer_container.size();
+    const Kmer_Container kmer_container(kmc_db_name);
+    const uint64_t kmer_count = kmer_container.size();
 
     std::cout << "Number of k-mers in the database: " << kmer_count << "\n";
 
@@ -93,6 +93,7 @@ bool Validator::validate_kmer_set() const
     uint64_t unitigs_processed = 0;
     uint64_t kmers_invalid = 0;
 
+    // Scan through the unitigs one-by-one.
     std::string unitig;
     std::ifstream input(cdbg_file_name.c_str(), std::ifstream::in);
     while(input >> unitig)
@@ -100,17 +101,22 @@ bool Validator::validate_kmer_set() const
         cuttlefish::kmer_t first_kmer(unitig, 0);
         Directed_Kmer kmer(first_kmer);
 
+        // Scan through the k-mers one-by-one.
         for(size_t kmer_idx = 0; kmer_idx <= unitig.length() - k; ++kmer_idx)
         {
             uint64_t hash_val = mph->lookup(kmer.canonical);
 
+            // Encountered a k-mer that is absent at the k-mer database and hashes outside of the valid range.
             if(hash_val >= kmer_count)
             {
                 std::cout << "Invalid k-mer encountered.\n";
                 kmers_invalid++;
             }
+            // Encountered a k-mer for the first time that is either a k-mer present at the database, or is
+            // absent there but hashes to the value of a present one (and that present one hasn't been seen yet).
             else if(!is_present[hash_val])
                 is_present[hash_val] = true;
+            // A repeated k-mer is seen.
             else
             {
                 std::cout << "Repeated k-mer encountered.\n";
@@ -123,8 +129,9 @@ bool Validator::validate_kmer_set() const
 
 
         kmers_seen += unitig.length() - k + 1;
+        unitigs_processed++;
 
-        if(++unitigs_processed % 1000000 == 0)
+        if(unitigs_processed % progress_grain_size == 0)
             std::cout << "Processed " << unitigs_processed / 1000000 << "M unitigs.\n";
     }
 
@@ -144,12 +151,13 @@ bool Validator::validate_sequence_completion()
 {
     std::cout << "Testing validation of the completeness of coverage of the sequence by the produced unitigs.\n";
 
-    Kmer_Container kmer_container(kmc_db_name);
-    uint64_t kmer_count = kmer_container.size();
+    const Kmer_Container kmer_container(kmc_db_name);
+    const uint64_t kmer_count = kmer_container.size();
 
     std::cout << "Number of k-mers in the k-mer database: " << kmer_count << "\n";
 
 
+    // Allocate the unitig tables.
     std::cout << "Allocating the unitig tables.\n";
 
     unitig_id.resize(kmer_count);
@@ -158,6 +166,7 @@ bool Validator::validate_sequence_completion()
     std::cout << "Done allocation of the unitig tables.\n";
 
 
+    // Load the unitigs into memory and build the associated tables `unitig_id` and `unitig_dir`.
     build_unitig_tables();
 
 
@@ -221,20 +230,20 @@ void Validator::build_unitig_tables()
 
         if(unitig.length() == k)
         {
-            cuttlefish::kmer_t kmer(unitig);
-            uint64_t kmer_hash = mph->lookup(kmer.canonical());
+            const cuttlefish::kmer_t kmer(unitig);
+            const uint64_t kmer_hash = mph->lookup(kmer.canonical());
             
             unitig_id[kmer_hash] = unitig_count;
             unitig_dir[kmer_hash] = Unitig_Dir::either;
         }
         else
         {
-            cuttlefish::kmer_t last_kmer = cuttlefish::kmer_t(unitig.substr(unitig.length() - k, k));
-            cuttlefish::kmer_t kmer_fwd(unitig, 0);
-            cuttlefish::kmer_t kmer_bwd = last_kmer.reverse_complement();
+            const cuttlefish::kmer_t last_kmer = cuttlefish::kmer_t(unitig.substr(unitig.length() - k, k));
+            const cuttlefish::kmer_t kmer_fwd(unitig, 0);
+            const cuttlefish::kmer_t kmer_bwd = last_kmer.reverse_complement();
 
-            uint64_t kmer_fwd_hash = mph->lookup(kmer_fwd.canonical());
-            uint64_t kmer_bwd_hash = mph->lookup(kmer_bwd.canonical());
+            const uint64_t kmer_fwd_hash = mph->lookup(kmer_fwd.canonical());
+            const uint64_t kmer_bwd_hash = mph->lookup(kmer_bwd.canonical());
             
             unitig_id[kmer_fwd_hash] = unitig_count;
             unitig_dir[kmer_fwd_hash] = Unitig_Dir::fwd;
@@ -243,10 +252,11 @@ void Validator::build_unitig_tables()
             unitig_dir[kmer_bwd_hash] = Unitig_Dir::bwd;
         }
 
+        
         unitig_count++;
         
         // Track progress.
-        if(unitig_count % 1000000 == 0)
+        if(unitig_count % progress_grain_size == 0)
             std::cout << "Loaded " << unitig_count / 1000000 << "M unitigs.\n";
     }
 
@@ -256,14 +266,14 @@ void Validator::build_unitig_tables()
 }
 
 
-bool Validator::walk_sequence(const char* seq, const size_t seq_len)
+bool Validator::walk_sequence(const char* seq, const size_t seq_len) const
 {
     size_t kmer_idx = 0;
     while(kmer_idx <= seq_len - k)
     {
         kmer_idx = search_valid_kmer(seq, seq_len, kmer_idx);
 
-        // No valid k-mer remains in the substring anymore.
+        // No valid k-mer remains in this sequence anymore.
         if(kmer_idx > seq_len - k)
             break;
 
@@ -286,7 +296,7 @@ size_t Validator::search_valid_kmer(const char* seq, const size_t seq_len, const
     while(idx <= seq_len - k)
     {
         // Go over the contiguous subsequence of 'N's.
-        for(; idx <= seq_len - k && seq[idx] == 'N'; idx++);
+        for(; idx <= seq_len - k && seq[idx] == cuttlefish::PLACEHOLDER_NUCLEOTIDE; idx++);
 
         // Go over the contiguous subsequence of non-'N's.
         if(idx <= seq_len - k)
@@ -294,7 +304,7 @@ size_t Validator::search_valid_kmer(const char* seq, const size_t seq_len, const
             valid_start_idx = idx;
             nucl_count = 0;
 
-            for(; idx < seq_len && seq[idx] != 'N'; ++idx)
+            for(; idx < seq_len && seq[idx] != cuttlefish::PLACEHOLDER_NUCLEOTIDE; ++idx)
                 if(++nucl_count == k)
                     return valid_start_idx;
         }
@@ -307,13 +317,13 @@ size_t Validator::search_valid_kmer(const char* seq, const size_t seq_len, const
 
 size_t Validator::walk_first_unitig(const char* seq, const size_t seq_len, const size_t start_idx) const
 {
-    cuttlefish::kmer_t kmer(seq, start_idx);
-    uint64_t kmer_hash = mph->lookup(kmer.canonical());
+    const cuttlefish::kmer_t kmer(seq, start_idx);
+    const uint64_t kmer_hash = mph->lookup(kmer.canonical());
     
     if(unitig_dir[kmer_hash] == Unitig_Dir::none)
     {
         std::cout << "Encountered k-mer(s) in sequence that are not flanking k-mers of any of the result unitigs,"
-                     "yet unitig traversals were attempted from those. Aborting.\n";
+                     " yet unitig traversals were attempted from those. Aborting.\n";
         return std::numeric_limits<size_t>::max();
     }
 
@@ -358,6 +368,5 @@ bool Validator::walk_unitig(const char* seq, const size_t seq_len, const size_t 
         if(start_idx + idx >= seq_len || seq[start_idx + idx] != Kmer::complement(unitig[len - 1 - idx]))
             return false;
             
-
     return true;
 }

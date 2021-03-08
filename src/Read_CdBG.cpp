94f593db99f9980e27652812be2c4048a6ddd92b
jamshed-k <jamshed@umd.edu>
2021-03-07 19:50:53 -0500

    Construct MPHF

--- src/Read_CdBG.cpp
+++ src/Read_CdBG.cpp
@@ -10,7 +10,13 @@ Read_CdBG<k>::Read_CdBG(const Build_Params& params):
 
 template <uint16_t k>
 void Read_CdBG<k>::construct()
-{}
+{
+    std::cout << "\nConstructing the minimal perfect hash function (MPHF) over the vertex set.\n";
+    hash_table.construct(params.vertex_db_path(), params.thread_count(), params.working_dir_path(), params.mph_file_path());
+
+
+    hash_table.clear();
+}
 
 
 

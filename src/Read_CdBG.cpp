cea18b04b01cc3538b6bb0998a79c8583c212313
jamshed-k <jamshed@umd.edu>
2021-03-08 20:31:27 -0500

    Help debug

--- src/Read_CdBG.cpp
+++ src/Read_CdBG.cpp
@@ -12,8 +12,8 @@ Read_CdBG<k>::Read_CdBG(const Build_Params& params):
 template <uint16_t k>
 void Read_CdBG<k>::construct()
 {
-    std::cout << "\nConstructing the minimal perfect hash function (MPHF) over the vertex set.\n";
-    hash_table.construct(params.vertex_db_path(), params.thread_count(), params.working_dir_path(), params.mph_file_path());
+    // std::cout << "\nConstructing the minimal perfect hash function (MPHF) over the vertex set.\n";
+    // hash_table.construct(params.vertex_db_path(), params.thread_count(), params.working_dir_path(), params.mph_file_path());
 
     std::cout << "\nComputing the DFA states.\n";
     Read_CdBG_Constructor<k> cdBg_constructor(params, hash_table);

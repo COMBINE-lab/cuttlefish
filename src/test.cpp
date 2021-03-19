127a6f6833a529228a1610cae7d110a81da01038
jamshed-k <jamshed@umd.edu>
2021-03-18 21:07:59 -0400

    Have no executing test code

--- src/test.cpp
+++ src/test.cpp
@@ -576,11 +576,11 @@ int main(int argc, char** argv)
 
     // count_kmers_in_unitigs(argv[1], atoi(argv[2]));
 
-    static constexpr uint16_t k = 26;
-    static const size_t consumer_count = std::atoi(argv[2]);
+    // static constexpr uint16_t k = 26;
+    // static const size_t consumer_count = std::atoi(argv[2]);
 
     // test_buffered_iterator_performance<k>(argv[1]);
-    test_SPMC_iterator_performance<k>(argv[1], consumer_count);
+    // test_SPMC_iterator_performance<k>(argv[1], consumer_count);
 
 
     return 0;

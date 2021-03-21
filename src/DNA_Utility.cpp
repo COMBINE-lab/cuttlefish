ffe1f3d43fad59649280bfd91ccb4d4736ab93d3
jamshed-k <jamshed@umd.edu>
2021-03-21 17:15:32 -0400

    Define reverse complement bytes

--- src/DNA_Utility.cpp
+++ src/DNA_Utility.cpp
@@ -6,3 +6,4 @@ constexpr DNA::Base DNA_Utility::MAPPED_BASE[128];
 constexpr DNA::Base DNA_Utility::COMPLEMENTED_BASE[5];
 constexpr char DNA_Utility::COMPLEMENTED_CHAR[128];
 constexpr bool DNA_Utility::IS_PLACEHOLDER[128];
+constexpr uint8_t DNA_Utility::REVERSE_COMPLEMENT_BYTE[256];

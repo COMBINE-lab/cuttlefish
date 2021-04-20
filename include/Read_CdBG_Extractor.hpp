ac92b1d18ce56a029238f5f4eb8841bc62f40210
jamshed-k <jamshed@umd.edu>
2021-04-19 21:55:28 -0400

    Add flanking status computer

--- include/Read_CdBG_Extractor.hpp
+++ include/Read_CdBG_Extractor.hpp
@@ -39,6 +39,26 @@ private:
     // vertex, and if it is, then piece-wise constructs the corresponding unipath.
     void process_vertices(Kmer_SPMC_Iterator<k>* vertex_parser, uint16_t thread_id);
 
+    // Returns `true` iff some vertex `v` with the provided state `state` is a flanking vertex for
+    // the maximal unitig containing it. If it is, then stores the side of `v` that is opposite to
+    // the flanking side, i.e. the side extending the unipath `p` containing `v`, to `unipath_side`.
+    // If `p` is trivial, then the side stored is implementation-specific.
+    // NB: unless for flanking vertices that are branching, this function cannot possibly be defined
+    // — for non-branching vertices, it's not possible to compute whether they are flanking solely
+    // from their states. Nevertheless, given that the information-discarding heuristic for branching
+    // k-mers has been implemented in the DFA states computation phase, this method correctly computes
+    // the flanking-status for some vertex from just its state.
+    static bool is_flanking_state(State_Read_Space state, cuttlefish::side_t& unipath_side);
+
+    // Returns `true` iff the vertex-side `side` for a vertex with state `state` flanks the maximal
+    // unitig containing the vertex.
+    // NB: this method is only applicable when information-discarding is propagated to the neighbors
+    // from branching vertices. In absence of the implementation for the heuristic, this function
+    // cannot possibly be defined from solely the parameters `state` and `side` — for non-branching
+    // vertices, it's not possible to compute whether some side of them they are flanking solely
+    // from their states — a vertex `v` is also required.
+    static bool is_flanking_side(State_Read_Space state, cuttlefish::side_t side);
+
 
 public:
 
@@ -49,6 +69,35 @@ public:
     // Extracts the maximal unitigs of the de Bruijn graph.
     void extract_maximal_unitigs();
 };
+
+
+template <uint16_t k>
+inline bool Read_CdBG_Extractor<k>::is_flanking_state(const State_Read_Space state, cuttlefish::side_t& unipath_side)
+{
+    if(is_flanking_side(state, cuttlefish::side_t::front))
+    {
+        unipath_side = cuttlefish::side_t::back;
+        return true;
+    }
+
+    if(is_flanking_side(state, cuttlefish::side_t::back))
+    {
+        unipath_side = cuttlefish::side_t::front;
+        return true;
+    }
+
+
+    return false;
+}
+
+
+template <uint16_t k>
+inline bool Read_CdBG_Extractor<k>::is_flanking_side(const State_Read_Space state, const cuttlefish::side_t side)
+{
+    const cuttlefish::edge_encoding_t edge = state.edge_at(side);
+
+    return edge == cuttlefish::edge_encoding_t::N || edge == cuttlefish::edge_encoding_t::E;
+}
 
 
 

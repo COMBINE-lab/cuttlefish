98890494af91cb8a7cbd36abaa047539430d7f7c
jamshed-k <jamshed@umd.edu>
2021-05-19 13:17:56 -0400

    Clean app driver header

--- include/Application.hpp
+++ include/Application.hpp
@@ -58,98 +58,16 @@ private:
 
 public:
 
-    Application(const Build_Params& params):
-        app(params.k() == 1 ? new T_App<1>(params) : nullptr),
-        validator(nullptr)
-    {}
+    Application(const Build_Params& params);
 
+    Application(const Validation_Params& params);
 
-    Application(const Validation_Params& params):
-        app(nullptr),
-        validator(params.k() == 1 ? new Validator<1>(params) : nullptr)
-    {}
+    ~Application();
 
+    void execute() const;
 
-    ~Application()
-    {
-        if(app != nullptr)
-            delete app;
-
-        if(validator != nullptr)
-            delete validator;
-    }
-
-
-    void execute() const
-    {
-        if(app != nullptr)
-            app->construct();
-        else
-        {
-            std::cerr << "The provided k is not valid. Aborting.\n";
-            std::exit(EXIT_FAILURE);
-        }
-    }
-
-
-    bool validate() const
-    {
-        if(validator != nullptr)
-            return validator->validate();
-
-        std::cerr << "The provided k is not valid. Aborting.\n";
-        std::exit(EXIT_FAILURE);
-    }
+    bool validate() const;
 };
-
-
-template <uint16_t k, template <uint16_t> typename T_App>
-inline Application<k, T_App>::Application(const Build_Params& params):
-    app_next_level(new Application<k - 2, T_App>(params)),
-    app(params.k() == k ? new T_App<k>(params) : nullptr),
-    validator(nullptr)
-{}
-
-
-template <uint16_t k, template <uint16_t> typename T_App>
-inline Application<k, T_App>::Application(const Validation_Params& params):
-    app_next_level(new Application<k - 2, T_App>(params)),
-    app(nullptr),
-    validator(params.k() == k ? new Validator<k>(params): nullptr)
-{}
-
-
-template <uint16_t k, template <uint16_t> typename T_App>
-inline Application<k, T_App>::~Application()
-{
-    delete app_next_level;
-
-    if(app != nullptr)
-        delete app;
-
-    if(validator != nullptr)
-        delete validator;
-}
-
-
-template <uint16_t k, template <uint16_t> typename T_App>
-inline void Application<k, T_App>::execute() const
-{
-    if(app != nullptr)
-        app->construct();
-    else
-        app_next_level->execute();
-}
-
-
-template <uint16_t k, template <uint16_t> typename T_App>
-inline bool Application<k, T_App>::validate() const
-{
-    if(validator != nullptr)
-        return validator->validate();
-
-    return app_next_level->validate();
-}
 
 
 

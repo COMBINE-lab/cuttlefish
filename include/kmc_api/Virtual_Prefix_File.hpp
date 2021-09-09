304b5ea49eb91ed64eb6d6a243c89c05b36473c6
jamshed <jamshed@umd.edu>
2021-09-09 16:28:23 -0400

    Better practice

--- include/kmc_api/Virtual_Prefix_File.hpp
+++ include/kmc_api/Virtual_Prefix_File.hpp
@@ -17,13 +17,13 @@ class Virtual_Prefix_File
 {
 private:
 
-	static constexpr size_t buffer_elem_count = (1 << 21);	// Number of prefixes to be kept in memory buffer at a time.
-	size_t prefix_file_elem_count;	// Size of the KMC3 prefix-file (*.kmc_pre) in elements (i.e. 64-bit prefixes).
+	static constexpr std::size_t buffer_elem_count = (1 << 21);	// Number of prefixes to be kept in memory buffer at a time.
+	std::size_t prefix_file_elem_count;	// Size of the KMC3 prefix-file (*.kmc_pre) in elements (i.e. 64-bit prefixes).
 	std::vector<uint64_t> prefix_file_buf;	// The in-memory prefix-file buffer.
 
 	uint64_t lut_area_size_in_bytes;	// From KMC3.
-	size_t prefix_chunk_start_index;	// The index into the prefix-file where the prefix chunk currently loaded into memory starts.
-	size_t prefix_chunk_end_index;	// The (non-inclusive) index into the prefix-file where the prefix chunk currently loaded into memory ends.
+	std::size_t prefix_chunk_start_index;	// The index into the prefix-file where the prefix chunk currently loaded into memory starts.
+	std::size_t prefix_chunk_end_index;	// The (non-inclusive) index into the prefix-file where the prefix chunk currently loaded into memory ends.
 
 	uint64_t total_kmers;	// Total number of k-mers in the KMC3 database.
 	std::FILE* fp;	// File handle to the KMC3 prefix-file.
@@ -31,7 +31,7 @@ private:
 
 	// Reads in as much data as possible from the prefix-file into the in-memory buffer,
 	// and returns the number of elements read.
-	size_t read_prefixes();
+	std::size_t read_prefixes();
 
 
 public:
@@ -54,15 +54,15 @@ public:
 	void init(std::FILE*& fptr, uint64_t lut_area_bytes, uint64_t kmer_count);
 
 	// Returns the data at index `idx` of the prefix-file.
-	uint64_t operator[](size_t idx);
+	uint64_t operator[](std::size_t idx);
 };
 
 
-inline size_t Virtual_Prefix_File::read_prefixes()
+inline std::size_t Virtual_Prefix_File::read_prefixes()
 {
-	const size_t elems_to_read = std::min(prefix_file_elem_count - prefix_chunk_end_index, buffer_elem_count);
-	const size_t bytes_to_read = elems_to_read * sizeof(uint64_t);
-	const size_t bytes_read = std::fread(prefix_file_buf.data(), 1, bytes_to_read, fp);
+	const std::size_t elems_to_read = std::min(prefix_file_elem_count - prefix_chunk_end_index, buffer_elem_count);
+	const std::size_t bytes_to_read = elems_to_read * sizeof(uint64_t);
+	const std::size_t bytes_read = std::fread(prefix_file_buf.data(), 1, bytes_to_read, fp);
 	
 	if(bytes_read != bytes_to_read)
 	{
@@ -74,7 +74,7 @@ inline size_t Virtual_Prefix_File::read_prefixes()
 }
 
 
-inline uint64_t Virtual_Prefix_File::operator[](const size_t idx)
+inline uint64_t Virtual_Prefix_File::operator[](const std::size_t idx)
 {
 	if(idx >= prefix_file_elem_count) 
 		return total_kmers + 1;

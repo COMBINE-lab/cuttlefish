58c8e409cfd6feca7825661eeecba709898a8fdd
jamshed <jamshed@umd.edu>
2022-01-31 14:40:30 -0500

    Bump default thread-count

--- include/Input_Defaults.hpp
+++ include/Input_Defaults.hpp
@@ -6,6 +6,8 @@
 
 #include "Output_Format.hpp"
 
+#include <thread>
+
 
 namespace cuttlefish
 {
@@ -16,7 +18,8 @@ namespace cuttlefish
         constexpr uint16_t K = 27;
         constexpr uint32_t CUTOFF_FREQ_READS = 2;   // Typical practice
         constexpr uint32_t CUTOFF_FREQ_REFS = 1;    // Typical assumption
-        constexpr uint16_t THREAD_COUNT = 1;
+        const uint16_t THREAD_COUNT = (std::thread::hardware_concurrency() ?
+                                        (std::thread::hardware_concurrency()/ 4) : 8);  // A quarter of the total thread-count.
         constexpr std::size_t MAX_MEMORY = 3;   // Set as per KMC3 stage 1 performance.
 #ifdef CF_DEVELOP_MODE
         constexpr double GAMMA = 0;

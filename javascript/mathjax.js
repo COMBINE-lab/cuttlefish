8ba6438b1a7f5f17ddcda922e4e595c7b8f0387f
rob-p <rob.patro@gmail.com>
2023-12-01 16:59:32 -0500

    Deployed 5552f63 with MkDocs version: 1.5.3

---
+++ javascript/mathjax.js
@@ -0,0 +1,19 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => { 
+
+
+  MathJax.typesetPromise()
+})
+

70b9647b5e5e23c7e8058e3809e94c7644921c5d
jamshed <jamshed@umd.edu>
2025-04-28 21:30:27 -0400

    Discontinue (partially) obsolete args

--- src/commands.cpp
+++ src/commands.cpp
@@ -1,14 +1,11 @@
 #include "Input_Defaults.hpp"
-#include "CdBG.hpp"
-#include "Read_CdBG.hpp"
 #include "dBG_Contractor.hpp"
-#include "Kmer_Index.hpp"
-#include "Validator.hpp"
+// #include "Kmer_Index.hpp"
 #include "Build_Params.hpp"
-#include "Validation_Params.hpp"
 #include "Application.hpp"
 #include "version.hpp"
 #include "cxxopts/cxxopts.hpp"
+#include "parlay/parallel.h"
 
 #include <string>
 #include <vector>
@@ -35,6 +32,7 @@ int cf_build(int argc, char** argv)
     std::optional<std::vector<std::string>> lists;
     std::optional<std::vector<std::string>> dirs;
     std::optional<std::size_t> max_memory;
+    std::optional<uint32_t> cutoff;
     options.add_options("common")
         ("s,seq", "input files",
             cxxopts::value<std::optional<std::vector<std::string>>>(seqs))
@@ -44,41 +42,45 @@ int cf_build(int argc, char** argv)
             cxxopts::value<std::optional<std::vector<std::string>>>(dirs))
         ("k,kmer-len", "k-mer length",
             cxxopts::value<uint16_t>()->default_value(std::to_string(cuttlefish::_default::K)))
-        ("t,threads", "number of threads to use",
-            cxxopts::value<uint16_t>()->default_value(std::to_string(cuttlefish::_default::THREAD_COUNT)))
-        ("idx", "construct a k-mer index of the de Bruijn graph")
+        // ("t,threads", "number of threads to use",
+        //     cxxopts::value<uint16_t>()->default_value(std::to_string(cuttlefish::_default::THREAD_COUNT)))
+        // ("idx", "construct a k-mer index of the de Bruijn graph")
         ("min-len", "minimizer length",
             cxxopts::value<uint16_t>()->default_value(std::to_string(cuttlefish::_default::MIN_LEN)))
         ("o,output", "output file",
             cxxopts::value<std::string>())
         ("w,work-dir", "working directory",
             cxxopts::value<std::string>()->default_value(cuttlefish::_default::WORK_DIR))
+/*
         ("m,max-memory", "soft maximum memory limit in GB (default: " + std::to_string(cuttlefish::_default::MAX_MEMORY) + ")",
             cxxopts::value<std::optional<std::size_t>>(max_memory))
         ("unrestrict-memory", "do not impose memory usage restriction")
-        ("h,help", "print usage")
         ;
 
-    std::optional<uint32_t> cutoff;
     options.add_options("cuttlefish_2")
         ("read", "construct a compacted read de Bruijn graph (for FASTQ input)")
         ("ref", "construct a compacted reference de Bruijn graph (for FASTA input)")
-        ("c,cutoff", "frequency cutoff for (k + 1)-mers (default: refs: " + std::to_string(cuttlefish::_default::CUTOFF_FREQ_REFS) + ", reads: " + std::to_string(cuttlefish::_default::CUTOFF_FREQ_READS) + ")",
-            cxxopts::value<std::optional<uint32_t>>(cutoff))
         ("path-cover", "extract a maximal path cover of the de Bruijn graph")
         ;
+*/
 
-    options.add_options("cuttlefish_3")
+    // options.add_options("cuttlefish_3")
+        ("c,cutoff", "frequency cutoff for (k + 1)-mers (default: refs: " + std::to_string(cuttlefish::_default::CUTOFF_FREQ_REFS) + ", reads: " + std::to_string(cuttlefish::_default::CUTOFF_FREQ_READS) + ")",
+            cxxopts::value<std::optional<uint32_t>>(cutoff))
         ("color", "whether to color the compacted graph or not")
+/*
         ("vertex-part-count", "number of vertex-partitions in the discontinuity graph; needs to be a power of 2",
             cxxopts::value<std::size_t>()->default_value(std::to_string(cuttlefish::_default::VERTEX_PART_COUNT)))
         ("lmtig-bucket-count", "number of buckets storing literal locally-maximal unitigs",
             cxxopts::value<std::size_t>()->default_value(std::to_string(cuttlefish::_default::LMTIG_BUCKET_COUNT)))
         ("gmtig-bucket-count", "number of buckets for global maximal unitigs",
             cxxopts::value<std::size_t>()->default_value(std::to_string(cuttlefish::_default::GMTIG_BUCKET_COUNT)))
+*/
+        ("h,help", "print usage")
         ;
 
     std::optional<uint16_t> format_code;
+/*
     options.add_options("cuttlefish_1")
         ("f,format", "output format (0: FASTA, 1: GFA 1.0, 2: GFA 2.0, 3: GFA-reduced)",
             cxxopts::value<std::optional<uint16_t>>(format_code))
@@ -101,6 +103,7 @@ int cf_build(int argc, char** argv)
         ("gamma", "gamma for the BBHash MPHF",
             cxxopts::value<double>()->default_value(std::to_string(cuttlefish::_default::GAMMA)))
 #endif
+*/
         ;
 
     try
@@ -116,12 +119,12 @@ int cf_build(int argc, char** argv)
         const auto is_ref_graph = result["ref"].as<bool>();
         const auto k = result["kmer-len"].as<uint16_t>();
         const auto color = result["color"].as<bool>();
-        const auto vertex_part_count = result["vertex-part-count"].as<std::size_t>();
-        const auto lmtig_bucket_count = result["lmtig-bucket-count"].as<std::size_t>();
-        const auto gmtig_bucket_count = result["gmtig-bucket-count"].as<std::size_t>();
+        const auto vertex_part_count = cuttlefish::_default::VERTEX_PART_COUNT;     // result["vertex-part-count"].as<std::size_t>();
+        const auto lmtig_bucket_count = cuttlefish::_default::LMTIG_BUCKET_COUNT;   // result["lmtig-bucket-count"].as<std::size_t>();
+        const auto gmtig_bucket_count = cuttlefish::_default::GMTIG_BUCKET_COUNT;   // result["gmtig-bucket-count"].as<std::size_t>();
         const auto vertex_db = result["vertex-set"].as<std::string>();
         const auto edge_db = result["edge-set"].as<std::string>();
-        const auto thread_count = result["threads"].as<uint16_t>();
+        // const auto thread_count = result["threads"].as<uint16_t>();
         const auto strict_memory = !result["unrestrict-memory"].as<bool>();
         const auto idx = result["idx"].as<bool>();
         const auto min_len = result["min-len"].as<uint16_t>();
@@ -143,8 +146,8 @@ int cf_build(int argc, char** argv)
                                     seqs, lists, dirs,
                                     k, cutoff,
                                     color,
-                                    subgraph_count, vertex_part_count, lmtig_bucket_count, gmtig_bucket_count,
-                                    vertex_db, edge_db, thread_count, max_memory, strict_memory,
+                                    vertex_part_count, lmtig_bucket_count, gmtig_bucket_count,
+                                    vertex_db, edge_db, parlay::num_workers(), max_memory, strict_memory,
                                     idx, min_len,
                                     output_file, format, track_short_seqs, poly_n_stretch, working_dir,
                                     path_cover,
